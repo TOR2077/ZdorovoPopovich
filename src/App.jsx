d372e8f8109c375e9094d529f1387e2b6ae35784
Sanya <lev.abdun@mail.ru>
2025-05-05 11:08:13 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import TelegramWebApp from '@telegram/web-app';
+
 
 import Home from './pages/Home';
 import Page2 from './pages/Page2';
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  // Импортируем основной компонент приложения
-import './index.css'; // Если у тебя есть стили, например, для приложения
+
 
 // Рендерим приложение в корневой элемент
 ReactDOM.createRoot(document.getElementById('root')).render(
