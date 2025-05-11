2fd0628f0bd9e82c476836ac1ccfa3f6bbefc29e
Sanya <lev.abdun@mail.ru>
2025-05-11 21:56:13 +0300

    Registration 0.6 fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,33 @@ import Page2 from './pages/Page2';
 import Registration from './pages/Registration';
 
 function App() {
-  // Страница регистрации всегда показывается
-  return <Registration />;
+  const [registered, setRegistered] = useState(!!localStorage.getItem('userProfile'));
+
+  const handleRegister = () => {
+    setRegistered(true);
+  };
+
+  useEffect(() => {
+    // Инициализация Telegram Web App
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+    }
+  }, []);
+
+  if (!registered) {
+    return <Registration onRegister={handleRegister} />;
+  }
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
