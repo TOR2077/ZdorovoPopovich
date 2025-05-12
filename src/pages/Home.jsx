ec74e0c9877079674a042b68d7b26d1fad17515a
Sanya <lev.abdun@mail.ru>
2025-05-13 01:10:44 +0300

    Main logo 1.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -124,7 +124,7 @@ function Home({ setDirection }) {
       <WeekCalendar value={selectedDate} onChange={setSelectedDate} />
 
       <div className="main-image-block user-data-block">
-        <img src={mainLogo} alt="Медведь со звёздами" style={{maxWidth: '100%', maxHeight: 180, display: 'block', margin: '0 auto'}} />
+        <img src={mainLogo} alt="Медведь со звёздами" style={{maxWidth: '100%', maxHeight: 200, display: 'block', margin: '0 auto'}} />
       </div>
 
       <Notes date={selectedDate} />
